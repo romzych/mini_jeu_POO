b8d19dcc8677f2111208a933b9f6c07031baba19
Roman Blondel <rblondel23@gmail.com>
2019-10-15 16:41:56 +0200

    exo 1 terminé debut exo 2

--- app.rb
+++ app.rb
@@ -4,4 +4,26 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+player1 = Player.new("Josianne")
+player2 = Player.new("José")
+
+puts "A ma gauche ........ #{player1.name}"
+puts "A ma droite ........ #{player2.name}"
+puts ""
+while player1.life_points > 0 && player2.life_points > 0
+  puts "Voici l'état de chaque joueur :"
+  puts "#{player1.name} avec #{player1.life_points}"
+  puts "#{player2.name} avec #{player2.life_points}"
+  puts ""
+  puts "#*#*#*#*#*#*#*FIGHT*#*#*#*#*#*#*#"
+  puts "Au tour de Josianne"
+  player1.attacks(player2)
+  if player2.life_points <= 0 
+    break
+  end
+  puts "Au tour de José"
+  player2.attacks(player1)
+  puts ""
+end
+
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 class Player
   attr_accessor :name, :life_points
   
-  def initialize(name, life_points = 10)
-    @name = name
-    @life_points = life_points
+  def initialize(name)
+    @name         = name
+    @life_points  = 10
   end
 
   def show_state
@@ -11,9 +11,9 @@ class Player
   end
 
   def gets_damage(dmg)
-    self.life_points = self.life_points - dmg
-    if self.life_points <= 0
-      self.life_points = 0
+    @life_points = @life_points - dmg
+    if @life_points <= 0
+      @life_points = 0
       puts "le joueur #{self.name} a été tué !"
     end
   end
@@ -28,5 +28,52 @@ class Player
 
   def compute_damage
     return rand(1..6)
+  end
+end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    @name         = name
+    @life_points  = 100
+    @weapon_level = 1
+  end
+
+  def show_state
+    puts "#{self.name} a #{self.life_points} points de vie et une arme de niveau #{self.weapon_level}"
+  end
+  
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+    lvl_weapon = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{lvl_weapon}"
+    if lvl_weapon > @weapon_level
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+      @weapon_level = lvl_weapon
+    else
+      puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  def search_health_pack
+    randomize_life = rand(1..6)
+    if randomize_life == 1
+      puts "Tu n'a rien trouvé"
+    elsif randomize_life >= 2 && randomize_life <= 5
+      puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+      @life_points = @life_points + 50
+      if @life_points > 100
+        @life_points = 100
+      end
+    elsif randomize_life == 6
+      puts "Waow, tu as trouvé un pack de +80 points de vie !"
+      if @life_points > 100
+        @life_points = 100
+      end
+    end
   end
 end
